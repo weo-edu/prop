f316aca4af68ed839558833f28a86b8221ec7064
Andrew Shaffer <darawk@gmail.com>
2015-03-27 23:08:56 -0700

    add .set method; add test for set method; remove chai and switch to assert style

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-module.exports = function(path) {
+module.exports = function prop(path) {
   var parts = path ? path.split('.') : [];
 
-  return function(obj) {
+  var get = function(obj) {
     return parts.reduce(function(memo, part) {
       return memo && memo[part];
     }, obj);
   };
+
+  get.set = function(obj, value) {
+    var base = prop(parts.slice(0, -1).join('.'))(obj);
+    var final = parts[parts.length - 1];
+    base[final] = value;
+  };
+
+  return get;
 };
--- test/index.js
+++ test/index.js
@@ -1,18 +1,31 @@
-var expect = require('chai').expect;
-var prop = require('../');
+var assert = require('assert');
+var prop = require('..');
 
 describe('prop', function() {
   it('should work', function() {
     var obj = {a: 1, b: {c: 2}};
 
-    expect(prop('a')(obj)).to.equal(1);
-    expect(prop('a.b')(obj)).to.equal(undefined);
-    expect(prop('b.c')(obj)).to.equal(2);
+    assert.equal(prop('a')(obj), 1);
+    assert.equal(prop('a.b')(obj), undefined);
+    assert.equal(prop('b.c')(obj), 2);
   });
 
   it('should return the object itself if empty string is passed', function() {
     var obj = {};
 
-    expect(prop('')(obj)).to.equal(obj);
+    assert.equal(prop('')(obj), obj);
+  });
+
+  it('should set properties', function() {
+    var obj = {a: 1, b: {c: 2}};
+
+    assert.equal(obj.a, 1);
+    prop('a').set(obj, 2);
+    assert.equal(obj.a, 2);
+
+    prop('b').set(obj, {d: 3});
+    assert.equal(obj.b.d, 3);
+    prop('b.c').set(obj, 4);
+    assert.equal(obj.b.c, 4);
   });
 });
